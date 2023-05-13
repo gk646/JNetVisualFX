1e2dcdce41f60c103be0a846b6fa3f7b8e81dfd0
gk646 <lukas.gilch@gmx.de>
2023-05-13 07:17:30 +0200

    git

--- src/main/java/gk646/jnet/neuralNetwork/Layer.java
+++ src/main/java/gk646/jnet/neuralNetwork/Layer.java
@@ -12,26 +12,26 @@ public final class Layer {
         neurons = Neuron.createNeurons(neuronCount);
     }
 
-    public static Layer[] createLayers(List<Integer> layerInfo) {
+    public static Layer[] createLayers(short[] layerInfo) {
         Network.logger.info("Creating layers");
 
-        byte layerCount = (byte) layerInfo.size();
-        if (layerCount < 1 || layerCount > layerInfo.size()) {
+        byte layerCount = (byte) layerInfo.length;
+        if (layerCount < 1 || layerCount > layerInfo.length) {
             throw new IllegalArgumentException("Invalid layerCount");
         }
 
         Layer[] temp = new Layer[layerCount];
 
         for (byte i = 0; i < layerCount; i++) {
-            temp[i] = new Layer(layerInfo.get(i).byteValue());
+            temp[i] = new Layer((byte) layerInfo[i]);
         }
         return temp;
     }
 
-    public static short[][][] createWeightMatrix(List<Integer> layerInfo) {
+    public static short[][][] createWeightMatrix(short[] layerInfo) {
         Network.logger.info("Creating weight matrix");
 
-        byte layerCount = (byte) layerInfo.size();
+        short layerCount = (short) layerInfo.length;
         if (layerCount < 2) {
             Network.logger.severe("Invalid layerCount. There should be at least two layers.");
             throw new IllegalArgumentException("Invalid layerCount. There should be at least two layers.");
@@ -41,7 +41,7 @@ public final class Layer {
         short[][][] temp = new short[layerCount - 1][][];
 
         for (byte i = 0; i < layerCount - 1; i++) {
-            temp[i] = new short[layerInfo.get(i)][layerInfo.get(i + 1)];
+            temp[i] = new short[layerInfo[i]][layerInfo[i+1]];
         }
         return temp;
     }
--- src/main/java/gk646/jnet/neuralNetwork/Network.java
+++ src/main/java/gk646/jnet/neuralNetwork/Network.java
@@ -4,26 +4,51 @@ import gk646.jnet.neuralNetwork.builder.ActivationFunction;
 import gk646.jnet.neuralNetwork.builder.NetworkBuilder;
 import gk646.jnet.neuralNetwork.exceptions.NetworkIntegrityException;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 public final class Network {
     public static final Logger logger = Logger.getLogger(Network.class.getName());
 
+    static {
+        Logger rootLogger = Logger.getLogger("");
+        Handler[] handlers = rootLogger.getHandlers();
+        if (handlers[0] instanceof ConsoleHandler) {
+            rootLogger.removeHandler(handlers[0]);
+        }
+
+        Handler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.ALL);
+        consoleHandler.setFormatter(new Formatter() {
+            @Override
+            public String format(LogRecord logRecord) {
+                return
+                        " [" + logRecord.getLevel() + "] " +
+                                logRecord.getMessage() + "\n";
+            }
+        });
+
+
+        logger.addHandler(consoleHandler);
+    }
+
     Layer[] layers;
     byte layerCount;
     byte outputLayerSize;
     short[][][] weightMatrix;
-    List<Integer> layerInfo;
+    short[] layerInfo;
     ActivationFunction activeFunc;
 
     public Network(NetworkBuilder networkBuilder) {
         this.layerInfo = networkBuilder.getLayerInfo();
-        this.layerCount = (byte) (layerInfo.size() - 1);
-        this.outputLayerSize = layerInfo.get(layerCount).byteValue();
+        this.layerCount = (byte) layerInfo.length;
+        this.outputLayerSize = (byte) layerInfo[layerCount - 1];
         this.activeFunc = networkBuilder.getActiveFunc();
-
 
         layers = Layer.createLayers(layerInfo);
         weightMatrix = Layer.createWeightMatrix(layerInfo);
@@ -32,37 +57,74 @@ public final class Network {
     }
 
 
-    public float[] forwardPass(float[] inputs) {
-        float[] layerInput = inputs;
-        for (byte i = 0; i < layerCount; i++) {
-            float[] layerOutput = new float[weightMatrix[i].length];
+    public void testInput(float[] inputs) {
+        if (inputs.length != layers[0].neuronCount) {
+            throw new IllegalArgumentException("Input array doesn't match the size of the input layer!");
+        }
+        System.out.println(Arrays.toString(forwardPass(inputs)));
+    }
 
-            for (byte j = 0; j < weightMatrix[i].length; j++) {
-                float neuronSum = 0;
-                for (byte k = 0; k < weightMatrix[i][j].length; k++) {
-                    neuronSum += layerInput[k] * weightMatrix[i][j][k];
-                }
-                layerOutput[j] = activeFunc.apply(neuronSum);
+    private float[] forwardPass(float[] inputs) {
+
+        float[] layerInput = inputs;
+
+        for (byte i = 0; i < layerCount - 1; ++i) {
+            float[] layerOutput = new float[layerInfo[i + 1]];
+
+            for (int j = 0; j < layerOutput.length; j++) {
+                float weightedSum = dotProduct(layerInput, weightMatrix[i][j]) + layers[i].neurons[j].bias;
+                layerOutput[j] = activeFunc.apply(weightedSum);
             }
             layerInput = layerOutput;
         }
         return layerInput;
     }
+    public void print3DArray(short[][][] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Slice " + i + ":");
+            for (int j = 0; j < array[i].length; j++) {
+                for (int k = 0; k < array[i][j].length; k++) {
+                    System.out.print(array[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+
+    private float dotProduct(float[] inputs, short[] weights) {
+        float result = 0;
+        for (int i = 0; i < inputs.length; i++) {
+            result += inputs[i] * weights[i];
+        }
+        return result;
+    }
+
+
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        int maxNeurons = Collections.max(layerInfo);
+        StringBuilder sb = new StringBuilder("\n");
+        int max = 0;
+        for (int i = 0; i < layerCount; i++) {
+            if (layerInfo[i] > max) {
+                max = layerInfo[i];
+            }
+        }
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < layerCount; j++) {
+                if (layerInfo[j] < max && i < max - layerInfo[j]) {
 
-        for (int i = 0; i < maxNeurons; i++) {
-            for (Integer neurons : layerInfo) {
-                if (neurons > i) {
-                    sb.append("O ");
+                }
+                if (i < layerInfo[j]) {
+                    sb.append("o ");
                 } else {
                     sb.append("  ");
                 }
             }
             sb.append("\n");
         }
+
         return sb.toString();
     }
 
@@ -70,13 +132,20 @@ public final class Network {
     private void networkIntegrityCheck() {
         logger.info("Performing network integrity checks");
         for (int i = 0; i < layerCount - 1; i++) {
-            if (weightMatrix[i].length != layerInfo.get(i) || weightMatrix[i][0].length != layerInfo.get(i + 1)) {
+            if (weightMatrix[i].length != layerInfo[i] || weightMatrix[i][0].length != layerInfo[i + 1]) {
                 logger.severe("Weight matrix dimensions don't match layerInfo!");
                 throw new NetworkIntegrityException("Weight matrix dimensions don't match layerInfo!");
             }
         }
+        logger.info("Printing network structure:");
+        System.out.println(this);
+        try {
+            Thread.sleep(25);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-
-        logger.info("Network integrity checks successful!");
+        print3DArray(new short[5][2][2]);
+        logger.info("Network integrity checks successful!\n");
     }
 }
