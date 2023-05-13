14c28f89dece2cc312046ad07eb5ab03c6350716
gk646 <lukas.gilch@gmx.de>
2023-05-13 21:52:22 +0200

    pre backpropagation commit

--- src/main/java/gk646/jnet/neuralNetwork/NetworkUtils.java
+++ src/main/java/gk646/jnet/neuralNetwork/NetworkUtils.java
@@ -1,5 +1,7 @@
 package gk646.jnet.neuralNetwork;
 
+import gk646.jnet.neuralNetwork.exceptions.NetworkIntegrityException;
+
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -7,37 +9,28 @@ import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
-public class NetworkUtils {
-    public static final Logger logger = Logger.getLogger(Network.class.getName());
+public final class NetworkUtils {
+    public static final Logger logger = Logger.getLogger(NetworkUtils.class.getName());
+    public static boolean verbose = true;
 
     static {
-        Logger rootLogger = Logger.getLogger("");
-        Handler[] handlers = rootLogger.getHandlers();
-        if (handlers[0] instanceof ConsoleHandler) {
-            rootLogger.removeHandler(handlers[0]);
-        }
-
-        Handler consoleHandler = new ConsoleHandler();
-        consoleHandler.setLevel(Level.ALL);
-        consoleHandler.setFormatter(new Formatter() {
-            @Override
-            public String format(LogRecord logRecord) {
-                return
-                        " [" + logRecord.getLevel() + "] " +
-                                logRecord.getMessage() + "\n";
-            }
-        });
-
-
-        logger.addHandler(consoleHandler);
+        initLogger();
     }
-    NetworkUtils(){
 
+    NetworkUtils() {
+    }
+
+    public static void removeHandler() {
+        Handler[] handlers = logger.getHandlers();
+        if (handlers[0] instanceof ConsoleHandler) {
+            logger.removeHandler(handlers[0]);
+        }
     }
 
     public void print3DArray(short[][][] array) {
+        if (!verbose) return;
         for (int i = 0; i < array.length; i++) {
-            System.out.println("Slice " + i + ":");
+            System.out.println("Weight Layer Pair " + i + ":");
             for (int j = 0; j < array[i].length; j++) {
                 for (int k = 0; k < array[i][j].length; k++) {
                     System.out.print(array[i][j][k] + " ");
@@ -48,12 +41,78 @@ public class NetworkUtils {
         }
     }
 
-    public void sleep(int millis){
+    public void sleep(int millis) {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+
+    public void printNetwork(Network network) {
+        if (!verbose) return;
+        StringBuilder sb = new StringBuilder("\n");
+        int max = 0;
+        for (int i = 0; i < network.layerCount; i++) {
+            if (network.layerInfo[i] > max) {
+                max = network.layerInfo[i];
+            }
+        }
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < network.layerCount; j++) {
+                if (i < network.layerInfo[j]) {
+                    sb.append("o ");
+                } else {
+                    sb.append("  ");
+                }
+            }
+            sb.append("\n");
         }
 
+        System.out.println(sb);
+    }
+
+
+    public void networkIntegrityCheck(Network network) {
+        logger.info("Performing network integrity checks");
+        for (int i = 0; i < network.layerCount - 1; i++) {
+            if (network.weightMatrix[i].length != network.layerInfo[i] || network.weightMatrix[i][0].length != network.layerInfo[i + 1]) {
+                logger.severe("Weight matrix dimensions don't match layerInfo!");
+                throw new NetworkIntegrityException("Weight matrix dimensions don't match layerInfo!");
+            }
+        }
+        logger.info("Printing network structure:");
+        network.netUtils.printNetwork(network);
+        network.netUtils.sleep(20);
+
+        logger.info("Printing weight matrix structure: \n");
+        network.netUtils.print3DArray(network.weightMatrix);
+        network.netUtils.sleep(20);
+
+        logger.info("Network integrity checks successful!\n");
+    }
+
+
+    private static void initLogger() {
+        Logger rootLogger = Logger.getLogger("");
+        Handler[] handlers = rootLogger.getHandlers();
+        if (handlers[0] instanceof ConsoleHandler) {
+            rootLogger.removeHandler(handlers[0]);
+        }
+        if (verbose) {
+            Handler consoleHandler = new ConsoleHandler();
+            consoleHandler.setLevel(Level.ALL);
+            consoleHandler.setFormatter(new Formatter() {
+                @Override
+                public String format(LogRecord logRecord) {
+                    return
+                            " [" + logRecord.getLevel() + "] " +
+                                    logRecord.getMessage() + "\n";
+                }
+            });
+
+            logger.addHandler(consoleHandler);
+        }
     }
 }
--- src/test/java/gk646/jnet/NetworkTest.java
+++ src/test/java/gk646/jnet/NetworkTest.java
@@ -1,23 +1,47 @@
 package gk646.jnet;
 
 import gk646.jnet.neuralNetwork.Network;
+import gk646.jnet.neuralNetwork.NetworkUtils;
+import gk646.jnet.neuralNetwork.NeuralNetwork;
 import gk646.jnet.neuralNetwork.builder.ActivationFunction;
 import gk646.jnet.neuralNetwork.builder.NetworkBuilder;
 import gk646.jnet.neuralNetwork.builder.NeuronInitState;
+import gk646.jnet.neuralNetwork.exceptions.IllegalNetworkArguments;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NetworkTest {
 
-
-    Network network = new Network(new NetworkBuilder(List.of(3, 3, 3), ActivationFunction.SIGMOID).setNeuronInitState(NeuronInitState.RANDOM));
+    static {
+        NetworkUtils.verbose = false;
+        NetworkUtils.removeHandler();
+    }
 
     @Test
-    void testNetworkOutput() {
-        assertTrue(Arrays.equals(new float[]{0.5f, 0.5f, 0.5f}, network.testInput(new float[]{3, 3, 3})));
+    void defaultOutputTest() {
+        var network = new NeuralNetwork(new NetworkBuilder(List.of(3, 3, 3), ActivationFunction.SIGMOID).setNeuronInitState(NeuronInitState.RANDOM));
+        assertArrayEquals(new float[]{0.5f, 0.5f, 0.5f}, network.testInput(new float[]{3, 3, 3}));
+    }
+
+    @Test
+    void networkStructureTest() {
+        var network = new NeuralNetwork(new NetworkBuilder(List.of(3, 3, 3), ActivationFunction.SIGMOID).setNeuronInitState(NeuronInitState.RANDOM));
+        network.testInput(new float[]{0.5f, 0.5f, 0.5f});
+
+        network = new NeuralNetwork(new NetworkBuilder(List.of(4, 3, 1), ActivationFunction.SIGMOID).setNeuronInitState(NeuronInitState.RANDOM));
+        network.testInput(new float[]{0.5f, 0.5f, 0.5f, 0.5f});
+
+        network = new NeuralNetwork(new NetworkBuilder(List.of(1, 4, 1), ActivationFunction.SIGMOID).setNeuronInitState(NeuronInitState.RANDOM));
+        network.testInput(new float[]{0.5f});
+
+        assertThrows(IllegalNetworkArguments.class, () -> new NetworkBuilder(List.of(-1, 3, 3), ActivationFunction.SIGMOID));
+
+        final var network1 = new NeuralNetwork(new NetworkBuilder(List.of(2, 4, 1), ActivationFunction.SIGMOID).setNeuronInitState(NeuronInitState.RANDOM));
+        assertThrows(InputMismatchException.class, () -> network1.testInput(new float[]{0.5f}));
     }
 }
