acd90e4ab0e01d68dd0a8fd56b5e435a281372a8
gk646 <lukas.gilch@gmx.de>
2023-05-20 23:16:49 +0200

    xor test is now passing with the new network implementation

--- src/test/java/gk646/jnet/network/SingleNeuronTest.java
+++ src/test/java/gk646/jnet/network/SingleNeuronTest.java
@@ -1,33 +1,29 @@
 package gk646.jnet.network;
 
-import gk646.jnet.neuralnetwork.NeuralNetwork;
-import gk646.jnet.neuralnetwork.builder.ActivationFunction;
-import gk646.jnet.neuralnetwork.builder.NetworkBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SingleNeuronTest {
-    double actiF(double x) {
-        return x;
+    float actiF(float x) {
+        return (float) (1 / (1 + Math.exp(-x)));
     }
 
-    double dactiF(double x) {
-        return 1;
+    float dactiF(float x) {
+        return actiF(x) * (1 - actiF(x));
     }
 
     static class Neuron {
-        double bias;
+        float bias;
 
-        Neuron(double bias) {
+        Neuron(float bias) {
             this.bias = bias;
         }
 
-        public static Neuron[] layer(int size, Supplier<Double> method) {
+        public static Neuron[] layer(int size, Supplier<Float> method) {
             var temp = new Neuron[size];
             for (int i = 0; i < temp.length; i++) {
                 temp[i] = new Neuron(method.get());
@@ -38,93 +34,117 @@ class SingleNeuronTest {
 
     class Layer {
         Neuron[] neurons;
-        double[] output;
-        double[] input;
-        double[][] weights;
+        float[] output;
+        float[] input;
+        float[][] weights;
+        float[][] dweights;
+        boolean isLastLayer = false;
 
         Layer(int inputNeurons, int outputNeurons) {
-            this.neurons = Neuron.layer(outputNeurons, () -> -1 + Math.random() * 2);
-            this.weights = new double[inputNeurons][outputNeurons];
-            this.output = new double[outputNeurons];
-            this.input = new double[inputNeurons];
-
+            this.neurons = Neuron.layer(outputNeurons, () -> (float) 0);
+            this.weights = new float[inputNeurons + 1][outputNeurons];
+            this.output = new float[outputNeurons];
+            this.input = new float[inputNeurons + 1];
+            this.dweights = new float[weights.length][outputNeurons];
             for (int i = 0; i < outputNeurons; i++) {
-                for (int j = 0; j < inputNeurons; j++) {
-                    this.weights[j][i] = -0.5 + Math.random();
+                for (int j = 0; j < inputNeurons + 1; j++) {
+                    this.weights[j][i] = (float) (-0.5 + Math.random());
                 }
             }
         }
 
-        private double[] forwardPass(double[] input) {
-            System.arraycopy(input, 0, this.input, 0, input.length);
+        private float[] forwardPass(float[] in) {
+            System.arraycopy(in, 0, this.input, 0, in.length);
+            input[input.length - 1] = 1;
+            //System.out.println(Arrays.toString(input));
             Arrays.fill(output, 0);
             for (int i = 0; i < output.length; i++) {
                 for (int j = 0; j < input.length; j++) {
-                    output[i] += weights[j][i];
+                    output[i] += weights[j][i] * input[j];
                 }
                 output[i] += neurons[i].bias;
-                output[i] = actiF(output[i]);
+                if (!isLastLayer) {
+                    output[i] = (float) (1 / (1 + Math.exp(-output[i])));
+                }
             }
             return Arrays.copyOf(output, output.length);
         }
 
-        public double[] train(double[] error, double learnRate, double momentum) {
-            return error;
+        public float[] train(float[] error, float learnRate, float momentum) {
+            float[] nextError = new float[input.length];
+            for (int i = 0; i < output.length; i++) {
+                float d = error[i];
+                if (!isLastLayer) {
+                    d *= output[i] * (1 - output[i]);
+                }
+                for (int j = 0; j < input.length; j++) {
+                    nextError[j] += weights[j][i] * d;
+                    float dw = input[j] * d * learnRate;
+                    weights[j][i] += dweights[j][i] * momentum + dw;
+                    dweights[j][i] = dw;
+                }
+            }
+            return nextError;
         }
     }
 
-    Layer[] layers = new Layer[]{new Layer(1, 1), new Layer(1, 1)};
+    Layer[] layers = new Layer[]{new Layer(2, 2), new Layer(2, 1)};
 
+
+    private float[] run(float[] input) {
+        float[] referenceInput = input;
+        for (int i = 0; i < layers.length; i++) {
+            referenceInput = layers[i].forwardPass(referenceInput);
+        }
+        return referenceInput;
+    }
+
+
+    private void train(float[] input, float[] target, float learnRate, float momentum) {
+        float[] calcError = run(input);
+        float[] error = new float[calcError.length];
+        for (int i = 0; i < error.length; i++) {
+            error[i] = target[i] - calcError[i];
+        }
+        for (int i = layers.length - 1; i >= 0; i--) {
+            error = layers[i].train(error, learnRate, momentum);
+        }
+    }
 
     @Test
     void twoNeuronsSquare() {
-        double[][] input = new double[][]{{2}, {4}, {6}, {8}};
-        double[][] output = new double[][]{{4}, {8}, {12}, {16}};
-        double learnRate = 0.5;
-        double momentum = 0.5;
-        // for (int i = 0; i < input.length; i++) {
-        double[] referenceInput = input[0];
-        for (int k = 0; k < layers.length; k++) {
-            referenceInput = layers[k].forwardPass(referenceInput);
-        }
-        // }
-
-        System.out.println(Arrays.toString(referenceInput));
-        /*
-        System.out.println(weight);
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < input.length; j++) {
-                double error;
-                double[] neuronsOutput = new double[neurons.length];
-                for (int k = 0; k < neurons.length; k++) {
-                    neuronsOutput[k] = actiF(input[j] * neurons[k]);
-                    System.out.println(neuronsOutput[k]);
-                    error = neuronsOutput[k] - output[j];
-                    double outputGradient = error * dactiF(neuronsOutput[k]);
-                    neurons[k] -= outputGradient * learnRate;
-                }
+        layers[layers.length - 1].isLastLayer = true;
+        float[][] input = new float[][]{new float[]{0, 0}, new float[]{0, 1}, new float[]{1, 0}, new float[]{1, 1}};
+        float[][] target = new float[][]{new float[]{0}, new float[]{1}, new float[]{1}, new float[]{0}};
+        float learnRate = 0.3f;
+        float momentum = 0.6f;
+        int repses = 500;
+        for (int reps = 0; reps < repses; reps++) {
+            for (int i = 0; i < input.length; i++) {
+                train(input[i], target[i], learnRate, momentum);
             }
-
-
         }
-
-         */
+        System.out.println(Arrays.toString(run(new float[]{1, 1})));
+        assertTrue(Math.abs(run(new float[]{1, 1})[0]) < 0.1);
+        assertTrue(Math.abs(1 - run(new float[]{1, 0})[0]) < 0.1);
+        assertTrue(Math.abs(run(new float[]{0, 0})[0]) < 0.1);
+        assertTrue(Math.abs(1 - run(new float[]{0, 1})[0]) < 0.1);
     }
 
     @Test
-    void singleNeuronDouble() {
-        double[] input = new double[]{2, 4, 6, 8};
-        double[] target = new double[]{4, 8, 12, 16};
-        double learnRate = 0.04;
-        double weight = -1 + Math.random() * 2;
-        double bias = -1 + Math.random() * 2;
+    void singleNeuronfloat() {
+        float[] input = new float[]{2, 4, 6, 8};
+        float[] target = new float[]{4, 8, 12, 16};
+        float learnRate = 0.04f;
+        float weight = (float) (-1 + Math.random() * 2);
+        float bias = (float) (-1 + Math.random() * 2);
 
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < input.length; j++) {
-                double weightedSum = input[j] * weight + bias;
-                double output = actiF(weightedSum);
-                double error = output - target[j];
-                double outputGradient = error * dactiF(weightedSum);
+                float weightedSum = input[j] * weight + bias;
+                float output = actiF(weightedSum);
+                float error = output - target[j];
+                float outputGradient = error * dactiF(weightedSum);
 
                 weight -= outputGradient * learnRate * input[j];
                 bias -= outputGradient * learnRate;
@@ -136,6 +156,6 @@ class SingleNeuronTest {
         assertTrue(Math.abs(8 - actiF(4 * weight + bias)) < 0.2);
 
 
-       // var network = new NeuralNetwork(new NetworkBuilder(List.of(1), ActivationFunction.SIGMOID));
+        // var network = new NeuralNetwork(new NetworkBuilder(List.of(1), ActivationFunction.SIGMOID));
     }
 }
